1c94b8f7c7c9781f9af00b6777f2398b633959b1
Kasundi Ranaweera <kasundiranaweera@gmail.com>
2026-09-03 14:32:15 +0530

    updated corsconfig

--- src/main/java/com/example/talentbridgeats/config/CorsConfig.java
+++ src/main/java/com/example/talentbridgeats/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:5174",
-                "https://application-tracking-system-frontend.vercel.app",
+                "https://application-tracking-system-fronten.vercel.app",
                 "https://application-tracking-system-frontend-git-main-kasundi.vercel.app"
         ));
         config.setAllowedMethods(List.of(
